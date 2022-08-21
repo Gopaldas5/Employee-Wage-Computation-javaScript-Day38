b8efa4ef83bd8c4b16c4c08f803c7b597754e3fb
GopalDas <gopalkrdas1997@gmail.com>
2022-08-22 00:17:07 +0530

    Added: Calculating the Calculating Wages till Number of Working Days or Total Working hour per month

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+function getWorkingHours(empCheck){
+//     switch(empCheck){
+//         case IS_PART_TIME:
+//             return PART_TIME_HOURS;
+//         case IS_FULL_TIME:
+//             return FULL_TIME_HOURS;
+//         default:
+//            return 0; 
+//     }
+// }
